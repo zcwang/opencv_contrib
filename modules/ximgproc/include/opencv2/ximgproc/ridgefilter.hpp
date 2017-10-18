048c628a5bec188d6be47ed126e3a9a2b257ee70
kushalvyaskv <kushalvyaskv@gmail.com>
2017-10-19 00:14:58 +0530

    cleaned code . soft tabs indentation

--- modules/ximgproc/include/opencv2/ximgproc/ridgefilter.hpp
+++ modules/ximgproc/include/opencv2/ximgproc/ridgefilter.hpp
@@ -12,36 +12,36 @@ OpenCV port by : Kushal Vyas (@kushalvyas), Venkatesh Vijaykumar(@venkateshvijay
 #include <opencv2/core.hpp>
 
 namespace cv{
-	namespace ximgproc{
-		//! @addtogroup ximgproc_filters
-		//! @{
-		class CV_EXPORTS_W RidgeDetectionFilter : public Algorithm{
-			/**
-			 @brief	Applies Ridge Detection Filter to an input image. Implements Ridge detection similar to the one in [Mathematica](http://reference.wolfram.com/language/ref/RidgeFilter.html) using the eigen values from the Hessian Matrix of the input image using Sobel Derivatives. Additional refinement can be done using Skeletonization and Binarization.
-			 */
-		public:
-			/**
-			@brief Create pointer to the Ridge detection filter.
-			@param ddepth  Specifies output image depth. Defualt is CV_32FC1
-			@param dx Order of derivative x. Default is 1
-			@param dy  Order of derivative y. Default is 1
-			@param ksize Sobel kernel size . Default is 3
-			@param out_dtype Converted format for output. Default is CV_8UC1
-			@param scale . scale value for derivative values . Default is 1
-			@param delta  . optional bias added to output. Default is 0
-			@param borderType. pixel extrapolation method. Default is BORDER_DEFAULT 
-			@see Sobel, threshold, getStructuringElement, morphologyEx.( for additional refinement)
-			 */
+namespace ximgproc{
+    //! @addtogroup ximgproc_filters
+    //! @{
+    class CV_EXPORTS_W RidgeDetectionFilter : public Algorithm{
+    /**
+    @brief  Applies Ridge Detection Filter to an input image. Implements Ridge detection similar to the one in [Mathematica](http://reference.wolfram.com/language/ref/RidgeFilter.html) using the eigen values from the Hessian Matrix of the input image using Sobel Derivatives. Additional refinement can be done using Skeletonization and Binarization.
+    */
+    public:
+    /**
+    @brief Create pointer to the Ridge detection filter.
+    @param ddepth  Specifies output image depth. Defualt is CV_32FC1
+    @param dx Order of derivative x. Default is 1
+    @param dy  Order of derivative y. Default is 1
+    @param ksize Sobel kernel size . Default is 3
+    @param out_dtype Converted format for output. Default is CV_8UC1
+    @param scale . scale value for derivative values . Default is 1
+    @param delta  . optional bias added to output. Default is 0
+    @param borderType. pixel extrapolation method. Default is BORDER_DEFAULT 
+    @see Sobel, threshold, getStructuringElement, morphologyEx.( for additional refinement)
+    */
 
-			CV_WRAP static Ptr<RidgeDetectionFilter> create(int ddepth = CV_32FC1, int dx=1, int dy=1, int ksize = 3, int out_dtype=CV_8UC1, double scale = 1, double delta = 0, int borderType = BORDER_DEFAULT);
-			/**
-			@brief Apply Ridge detection filter on input image.
-			@param img. InputArray as supported by Sobel. img can be 1-Channel or 3-Channels.
-			@param out. OutputAray of structure as RidgeDetectionFilter::ddepth. Output image with ridges.  
-			 */
-			CV_WRAP virtual void getRidgeFilteredImage(InputArray _img, OutputArray out) = 0;
-		};
-	}
+    CV_WRAP static Ptr<RidgeDetectionFilter> create(int ddepth = CV_32FC1, int dx=1, int dy=1, int ksize = 3, int out_dtype=CV_8UC1, double scale = 1, double delta = 0, int borderType = BORDER_DEFAULT);
+    /**
+    @brief Apply Ridge detection filter on input image.
+    @param img. InputArray as supported by Sobel. img can be 1-Channel or 3-Channels.
+    @param out. OutputAray of structure as RidgeDetectionFilter::ddepth. Output image with ridges.  
+    */
+    CV_WRAP virtual void getRidgeFilteredImage(InputArray _img, OutputArray out) = 0;
+};
+}
 }
 
 #endif
--- modules/ximgproc/src/ridgedetectionfilter.cpp
+++ modules/ximgproc/src/ridgedetectionfilter.cpp
@@ -8,84 +8,84 @@
 
 
 namespace cv{
-	namespace ximgproc{
+    namespace ximgproc{
 
-		class RidgeDetectionFilterImpl : public RidgeDetectionFilter{
-			public:
-				int _ddepth, _dx, _dy, _ksize;
-				double _scale, _delta;
-				int _borderType;
-				int _out_dtype;
-				RidgeDetectionFilterImpl(int ddepth=CV_32FC1, int dx=1, int dy=1, int ksize = 3, int out_dtype=CV_8UC1, double scale = 1, double delta = 0, int borderType = BORDER_DEFAULT) {
-					CV_Assert((ksize == 1 || ksize == 3 || ksize == 5 || ksize == 7));
-					CV_Assert((ddepth == CV_32FC1 || ddepth == CV_64FC1));
-					_ddepth = ddepth;
-					_dx = dx;
-					_dy = dy;
-					_ksize = ksize;
-					_scale = scale;
-					_delta = delta;
-					_borderType = borderType;
-					_out_dtype = out_dtype;
-				}
-				virtual void getRidgeFilteredImage(InputArray _img, OutputArray out);
-			private:
-				virtual void getSobelX(InputArray img, OutputArray out);
-				virtual void getSobelY(InputArray img, OutputArray out);
-		};
+        class RidgeDetectionFilterImpl : public RidgeDetectionFilter{
+            public:
+                int _ddepth, _dx, _dy, _ksize;
+                double _scale, _delta;
+                int _borderType;
+                int _out_dtype;
+                RidgeDetectionFilterImpl(int ddepth=CV_32FC1, int dx=1, int dy=1, int ksize = 3, int out_dtype=CV_8UC1, double scale = 1, double delta = 0, int borderType = BORDER_DEFAULT) {
+                    CV_Assert((ksize == 1 || ksize == 3 || ksize == 5 || ksize == 7));
+                    CV_Assert((ddepth == CV_32FC1 || ddepth == CV_64FC1));
+                    _ddepth = ddepth;
+                    _dx = dx;
+                    _dy = dy;
+                    _ksize = ksize;
+                    _scale = scale;
+                    _delta = delta;
+                    _borderType = borderType;
+                    _out_dtype = out_dtype;
+                }
+                virtual void getRidgeFilteredImage(InputArray _img, OutputArray out);
+            private:
+                virtual void getSobelX(InputArray img, OutputArray out);
+                virtual void getSobelY(InputArray img, OutputArray out);
+        };
 
-		void RidgeDetectionFilterImpl::getSobelX(InputArray img, OutputArray out){
-			out.create(img.size(), _ddepth);
-			Sobel(img, out, _ddepth, _dx, 0, _ksize, _scale, _delta, _borderType);
-		}
+        void RidgeDetectionFilterImpl::getSobelX(InputArray img, OutputArray out){
+            out.create(img.size(), _ddepth);
+            Sobel(img, out, _ddepth, _dx, 0, _ksize, _scale, _delta, _borderType);
+        }
 
-		void RidgeDetectionFilterImpl::getSobelY(InputArray img, OutputArray out){
-			out.create(img.size(), _ddepth);
-			Sobel(img, out, _ddepth, 0, _dy, _ksize, _scale, _delta, _borderType);
-		}
+        void RidgeDetectionFilterImpl::getSobelY(InputArray img, OutputArray out){
+            out.create(img.size(), _ddepth);
+            Sobel(img, out, _ddepth, 0, _dy, _ksize, _scale, _delta, _borderType);
+        }
 
-		void RidgeDetectionFilterImpl::getRidgeFilteredImage(InputArray _img, OutputArray out){
-			Mat img = _img.getMat();
-			CV_Assert(img.channels() == 1 || img.channels() == 3);
+        void RidgeDetectionFilterImpl::getRidgeFilteredImage(InputArray _img, OutputArray out){
+            Mat img = _img.getMat();
+            CV_Assert(img.channels() == 1 || img.channels() == 3);
 
-			if(img.channels() == 3){
-				cvtColor(img, img, COLOR_BGR2GRAY);
-			}
+            if(img.channels() == 3){
+                cvtColor(img, img, COLOR_BGR2GRAY);
+            }
 
-			Mat sbx, sby;
-			getSobelX(img, sbx);
-			getSobelY(img, sby);
+            Mat sbx, sby;
+            getSobelX(img, sbx);
+            getSobelY(img, sby);
 
-			Mat sbxx, sbyy, sbxy;
-			getSobelX(sbx, sbxx);
-			getSobelY(sby, sbyy);
-			getSobelY(sbx, sbxy);
+            Mat sbxx, sbyy, sbxy;
+            getSobelX(sbx, sbxx);
+            getSobelY(sby, sbyy);
+            getSobelY(sbx, sbxy);
 
-			Mat sb2xx, sb2yy, sb2xy;
-			multiply(sbxx, sbxx, sb2xx);
-			multiply(sbyy, sbyy, sb2yy);
-			multiply(sbxy, sbxy, sb2xy);
+            Mat sb2xx, sb2yy, sb2xy;
+            multiply(sbxx, sbxx, sb2xx);
+            multiply(sbyy, sbyy, sb2yy);
+            multiply(sbxy, sbxy, sb2xy);
 
-			Mat sbxxyy;
-			multiply(sbxx, sbyy, sbxxyy);
+            Mat sbxxyy;
+            multiply(sbxx, sbyy, sbxxyy);
 
-			Mat rootex;
-			rootex = (sb2xx +  (sb2xy + sb2xy + sb2xy + sb2xy)  - (sbxxyy + sbxxyy) + sb2xy );
-			Mat root;
-			sqrt(rootex, root);
-			Mat ridgexp;
-			ridgexp = ( (sbxx + sbyy) + root );
-			ridgexp /= 2;
-			// ridgexp.copyTo(out);
-			ridgexp.convertTo(out, _out_dtype);
-		}
+            Mat rootex;
+            rootex = (sb2xx +  (sb2xy + sb2xy + sb2xy + sb2xy)  - (sbxxyy + sbxxyy) + sb2xy );
+            Mat root;
+            sqrt(rootex, root);
+            Mat ridgexp;
+            ridgexp = ( (sbxx + sbyy) + root );
+            ridgexp /= 2;
+            // ridgexp.copyTo(out);
+            ridgexp.convertTo(out, _out_dtype);
+        }
 
-	    Ptr<RidgeDetectionFilter> RidgeDetectionFilter::create(int ddepth , int dx, int dy, int ksize, int out_dtype, double scale , double delta, int borderType){
-	        return makePtr<RidgeDetectionFilterImpl>(RidgeDetectionFilterImpl( ddepth, dx, dy,  ksize, out_dtype , scale,  delta,  borderType));
-	    }
+        Ptr<RidgeDetectionFilter> RidgeDetectionFilter::create(int ddepth , int dx, int dy, int ksize, int out_dtype, double scale , double delta, int borderType){
+            return makePtr<RidgeDetectionFilterImpl>(RidgeDetectionFilterImpl( ddepth, dx, dy,  ksize, out_dtype , scale,  delta,  borderType));
+        }
 
-	}
-	
+    }
+    
 }
 
 
